b633d3616b8951aeb125ec67f6bc973aa41ffd9a
Alex Karachun <alexkarachun42@gmail.com>
2023-01-23 19:35:03 +0200

    HW #25

--- src/telran/recursion/LinearRecursion.java
+++ src/telran/recursion/LinearRecursion.java
@@ -1,5 +1,88 @@
 package telran.recursion;
 
 public class LinearRecursion {
+	static public long factorial(int n) {
+		long res = 0;
+		if (n < 0) {
+			res = factorial(-n);
+		} else if (n < 2) {
+			res = 1;
+		} else {
+			res = n * factorial(n - 1);
+		}
+		return res;
+	}
 
+	static public int power(int number, int power) {
+		int res = 1;
+		if (power < 0) {
+			throw new IllegalArgumentException();
+		}
+		if (power > 1) {
+			res = multiply(power(number, power - 1), number);
+		} else if (power == 1) {
+			res = number;
+		}
+		return res;
+	}
+
+	private static int multiply(int number, int multiplier) {
+		int res = 0;
+		if (multiplier < 0) {
+			res = -multiply(number, -multiplier);
+		} else if (multiplier > 0) {
+			res = number + multiply(number, multiplier - 1);
+		}
+		return res;
+	}
+
+	static public long sum(int ar[]) {
+		return sum(0, ar);
+	}
+
+	private static long sum(int firstIndex, int[] ar) {
+		long res = 0;
+		if (firstIndex < ar.length) {
+			res = ar[firstIndex] + sum(firstIndex + 1, ar);
+		}
+		return res;
+	}
+
+	public static long square(int x) {
+		x = x < 0 ? -x : x;
+		return x == 0 ? 0 : (x + x - 1 + square(--x));
+	}
+
+	public static void reverse(int[] ar) {
+		reverse(0, ar.length - 1, ar);
+	}
+
+	private static void reverse(int firstIndex, int lastIndex, int[] ar) {
+		if (firstIndex < lastIndex) {
+			swap(ar, firstIndex, lastIndex);
+			reverse(firstIndex + 1, lastIndex - 1, ar);
+		}
+	}
+
+	private static void swap(int[] ar, int firstIndex, int lastIndex) {
+		int tmp = ar[firstIndex];
+		ar[firstIndex] = ar[lastIndex];
+		ar[lastIndex] = tmp;
+	}
+
+	public static boolean isSubstring(String string, String pattern) {
+		return isSubstring(string, pattern, 0);
+	}
+
+	private static boolean isSubstring(String str, String pattern, int i) {
+		if (i == pattern.length()) {
+			return true;
+		} else if (str.length() == 0) {
+			return false;
+		} else if (str.charAt(0) == pattern.charAt(i)) {
+			return isSubstring(str.substring(1), pattern, ++i);
+		} else {
+			return isSubstring(str.substring(1), pattern, 0);
+		}
+	}
 }
--- src/telran/recursion/test/LinearRecursionTest.java
+++ src/telran/recursion/test/LinearRecursionTest.java
@@ -1,14 +1,93 @@
 package telran.recursion.test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static telran.recursion.LinearRecursion.*;
 
 import org.junit.jupiter.api.Test;
 
-class LinearRecursionTest {
+public class LinearRecursionTest {
+	int[] numbers = { 3, 5, -7, 11, 13, -17, 19, -23 };
+	int[] powers = { 7, 6, 5, 4, 3, 2, 1, 4 };
 
 	@Test
-	void test() {
+	void fTest() {
+		f(6);
 	}
-	
 
+	void f(int a) {
+		if (a > 5) {
+			f(a - 1);
+		}
+	}
+
+	@Test
+	void factorialTest() {
+		assertEquals(24, factorial(4));
+		assertEquals(24 * 5 * 6, factorial(6));
+		assertEquals(24, factorial(-4));
+	}
+
+	@Test
+	void powerTest() {
+		for (int i = 0; i < numbers.length; i++) {
+			int number = numbers[i], power = powers[i];
+			assertEquals(Math.pow(number, power), power(number, power));
+		}
+
+		assertEquals(1, power(1000, 0));
+		assertThrowsExactly(IllegalArgumentException.class, () -> power(1000, -1));
+		assertEquals(1000, power(10, 3));
+		assertEquals(-1000, power(-10, 3));
+		assertEquals(256, power(-2, 8));
+		assertEquals(-128, power(-2, 7));
+	}
+
+	@Test
+	void sumTest() {
+		int ar[] = { 1, 2, 3, 4, 5, 6 };
+		assertEquals(21, sum(ar));
+		assertEquals(0, sum(new int[0]));
+	}
+
+	@Test
+	void reverseTest() {
+		int ar[] = { 1, 2, 3, 4, 5, 6 };
+		int expected[] = { 6, 5, 4, 3, 2, 1 };
+		int ar1[] = { 1, 2, 3, 4, 5, 6, 7 };
+		int expected1[] = { 7, 6, 5, 4, 3, 2, 1 };
+		reverse(ar);
+		reverse(ar1);
+		assertArrayEquals(expected, ar);
+		assertArrayEquals(expected1, ar1);
+
+	}
+
+	@Test
+	void squareTest() {
+		assertEquals(0, square(0));
+		assertEquals(1, square(1));
+		assertEquals(4, square(2));
+		assertEquals(9, square(3));
+		assertEquals(16, square(4));
+		assertEquals(25, square(5));
+		assertEquals(36, square(-6));
+		assertEquals(49, square(-7));
+	}
+
+	@Test
+	void subStringTest() {
+		String str = "qwerty123qwe4";
+		assertTrue(isSubstring(str, ""));
+		assertTrue(isSubstring(str, "ty1"));
+		assertTrue(isSubstring(str, "q"));
+		assertTrue(isSubstring(str, "qwerty123qwe4"));
+		assertTrue(isSubstring(str, "qwe4"));
+		assertTrue(isSubstring("", ""));
+		assertFalse(isSubstring(str, "234"));
+		assertFalse(isSubstring(str, "876"));
+		assertFalse(isSubstring(str, "qwert1234"));
+		assertFalse(isSubstring(str, "qwer1"));
+		assertFalse(isSubstring(str, "qwe3"));
+		assertFalse(isSubstring("", "q"));
+	}
 }
